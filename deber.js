b87f040ef08789e6c5e6ad55fad140808034a900
st3benm <stevenmontalvan@gmail.com>
2025-10-28 09:24:56 -0500

    ""2

--- deber.js
+++ deber.js
@@ -3,7 +3,8 @@ function Letras() {
     console.log(`Ana tiene ${empleado.nombre.length} letras `)
 
 }
-function vocal(letra) {
+function vocal() {
+    let letra = "o"
     const producto = { nombre: "Programador", precio: 100 }
     let cv = 0;
     let nombre = producto.nombre
@@ -19,20 +20,52 @@ function vocal(letra) {
 
 }
 
-let letra = "o"
-vocal(letra);
+
+
 
 function palindroma() {
     const persona = { nombre: "Carlos", edad: 30 }
-    let nombre=persona.nombre
-    invertido="";
-    for(let c=nombre.length-1 ;c>=0;c--){
-    invertido+=nombre[c];
+    let nombre = persona.nombre
+    invertido = "";
+    for (let c = nombre.length - 1; c >= 0; c--) {
+        invertido += nombre[c];
     }
-    if(nombre===invertido){
+    if (nombre === invertido) {
         console.log(`Su palabra ${nombre} es invertida`)
-    } else{
+    } else {
         console.log(`Su palabra ${nombre} no es invertido`)
     }
 
 }
+function longitud() {
+    const empleado1 = { nombre: "María", edad: 28 }
+    const empleado2 = { nombre: "Guillermo", edad: 35 }
+    let nombre1 = empleado1.nombre
+    let nombre2 = empleado2.nombre
+    if (nombre1 > nombre2) {
+        console.log(`El nombre ${nombre1} es mas largo con ${nombre1.length}`)
+    } else {
+        console.log(`El nombre ${nombre2}es mas largo con ${nombre2.length}`)
+    }
+
+
+}
+
+function inicial() {
+    const empleado = {cargo: "Director General Académico" }
+    let nombreCompleto = empleado.cargo
+    let iniciales=nombreCompleto[0]+ "."
+    for (let c = 1; c < nombreCompleto.length; c++) {
+        if (nombreCompleto[c-1] === ' ') {
+            iniciales+=nombreCompleto[c]+"."
+
+
+            
+        }
+
+    }
+    console.log(`Las iniciales son ${iniciales}`)
+
+
+}
+//BLOQUE 2 
